0b945b5b50bb4a1da34fb1cbb647161c56346097
styxx3542 <madanpulkit67@gmail.com>
2023-11-30 11:30:39 +0530

    day 6 refactor

--- day6b/src/main.rs
+++ day6b/src/main.rs
@@ -1,12 +1,15 @@
+use std::collections::HashSet;
 use std::collections::VecDeque;
-
 fn get_start_position(s: &str) -> usize {
+    let mut set: HashSet<char> = HashSet::new();
     let mut v: VecDeque<char> = VecDeque::new();
     for (i, c) in s.chars().filter(|c| c.is_alphabetic()).enumerate() {
-        while v.contains(&c) {
-            v.pop_front();
+        while set.contains(&c) {
+            let c = v.pop_front().unwrap();
+            set.remove(&c);
         }
         v.push_back(c);
+        set.insert(c);
         if v.len() == 14 {
             return i + 1;
         }
