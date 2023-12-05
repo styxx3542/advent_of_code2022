1bb85796a3b741dc88e47b62de0c28da8afa2a2b
styxx3542 <madanpulkit67@gmail.com>
2023-12-05 06:32:18 +0530

    day 10

--- day10a/src/main.rs
+++ day10a/src/main.rs
@@ -1,3 +1,30 @@
+fn process(instruction: &str, x: i32) -> (u32, i32) {
+    if instruction.contains("noop") {
+        return (1, x);
+    }
+    let (_, value) = instruction.split_once(' ').unwrap();
+    let value: i32 = value.parse().unwrap();
+    (2, x + value)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let cycles = vec![20, 60, 100, 140, 180, 220];
+    let mut time: u32 = 0;
+    let mut value: i32 = 1;
+    let mut index: usize = 0;
+    let mut sum = 0;
+    for line in input.lines() {
+        let (inc, x) = process(line, value);
+        time += inc;
+        if time >= cycles[index] {
+            sum += value * cycles[index] as i32;
+            index += 1;
+        }
+        if index == cycles.len() {
+            break;
+        }
+        value = x;
+    }
+    println!("{sum}");
 }
--- day10b/src/main.rs
+++ day10b/src/main.rs
@@ -1,3 +1,33 @@
+fn process(instruction: &str, x: i32) -> (u32, i32) {
+    if instruction.contains("noop") {
+        return (0, x);
+    }
+    let (_, value) = instruction.split_once(' ').unwrap();
+    let value: i32 = value.parse().unwrap();
+    (1, x + value)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = include_str!("../input.txt").lines();
+    let mut wait_time = 0;
+    let mut value = 1;
+    let mut next_pos = 1;
+    for i in 0..240 {
+        let cycle = i % 40;
+        if cycle == 0 {
+            println!();
+        }
+        if wait_time != 0 {
+            wait_time -= 1;
+        } else {
+            value = next_pos;
+            (wait_time, next_pos) = process(input.next().unwrap(), value);
+        }
+        if (value - cycle).abs() < 2 {
+            print!("#");
+        } else {
+            print!(" ");
+        }
+    }
+    println!();
 }
