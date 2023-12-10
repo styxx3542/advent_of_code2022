27c20a5011a09fad29df8e16e6899a2b2aebbb03
styxx3542 <madanpulkit67@gmail.com>
2023-12-10 12:21:20 +0530

    day 13(real proud of this one)

--- day13b/src/main.rs
+++ day13b/src/main.rs
@@ -1,3 +1,18 @@
+mod parse;
+use nom::{combinator::all_consuming, Finish};
+use parse::{parse_input, Element};
 fn main() {
-    println!("Hello, world!");
+    let mut packets =
+        all_consuming(parse_input)(concat!(include_str!("../input.txt"), "\n[[2]]\n[[6]]"))
+            .finish()
+            .unwrap()
+            .1;
+    packets.sort();
+    let l = packets
+        .binary_search(&vec![Element::List(vec![Element::Integer(2)])])
+        .unwrap();
+    let r = packets
+        .binary_search(&vec![Element::List(vec![Element::Integer(6)])])
+        .unwrap();
+    println!("{}", (l + 1) * (r + 1));
 }
---
+++ day13b/src/parse.rs
@@ -0,0 +1,55 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete as cc,
+    character::complete::{one_of, space0, space1},
+    combinator::{map, value},
+    error::ParseError,
+    multi::{separated_list0, separated_list1},
+    sequence::{preceded, tuple},
+    IResult,
+};
+use std::cmp::Ordering;
+#[derive(PartialEq, Eq, Debug)]
+pub enum Element {
+    Integer(u32),
+    List(Vec<Element>),
+}
+
+impl PartialOrd for Element {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Element {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Element::Integer(l), Element::Integer(r)) => l.cmp(&r),
+            (Element::List(l), Element::List(r)) => l.cmp(&r),
+            (Element::Integer(l), Element::List(r)) => vec![Element::Integer(*l)].cmp(&r),
+            (Element::List(l), Element::Integer(r)) => l.cmp(&vec![Element::Integer(*r)]),
+        }
+    }
+}
+
+/*
+[[],[[],[[5,4]],10],[1]]
+[[[],3,1,[[4],3,0,[2,2]]],[[[0,3,4]],[0,2,[4,10,1,5]]],[],[6,[],9]]
+*/
+
+fn parse_list(s: &str) -> IResult<&str, Vec<Element>> {
+    let (s, (_, v, _)) = tuple((tag("["), separated_list0(tag(","), parse_element), tag("]")))(s)?;
+    Ok((s, v))
+}
+
+fn parse_element(s: &str) -> IResult<&str, Element> {
+    alt((
+        map(cc::u32, Element::Integer),
+        map(parse_list, Element::List),
+    ))(s)
+}
+
+pub fn parse_input(s: &str) -> IResult<&str, Vec<Vec<Element>>> {
+    separated_list1(cc::multispace1, parse_list)(s)
+}
